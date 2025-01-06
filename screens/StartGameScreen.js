e2df228a57c5f1dbf2956ef03cbe404c4d084df8
Donovan Ferrel <dferrel@tsts.com>
2025-01-06 10:46:39 -0600

    Code changes to configure the TextInput field

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -5,7 +5,13 @@ import PrimaryButton from "../components/PrimaryButton";
 function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput style={styles.numberInput} maxLength={2} />
+      <TextInput
+        style={styles.numberInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>Reset</PrimaryButton>
       <PrimaryButton>Confirm</PrimaryButton>
     </View>
@@ -19,23 +25,23 @@ const styles = StyleSheet.create({
     marginTop: 100,
     marginHorizontal: 24,
     padding: 16,
-    backgroundColor: '#72063c',
+    backgroundColor: "#72063c",
     borderRadius: 8,
     elevation: 4, // This is an Android specific property to apply shadow affect
-    shadowColor: 'black', // This is an iOS specific property to apply shadow affect
+    shadowColor: "black", // This is an iOS specific property to apply shadow affect
     shadowOffset: { width: 0, height: 2 }, // This is an iOS specific property to apply shadow affect
     shadowRadius: 6, // This is an iOS specific property to apply shadow affect
-    shadowOpacity: 0.25,// This is an iOS specific property to apply shadow affect
+    shadowOpacity: 0.25, // This is an iOS specific property to apply shadow affect
   },
   numberInput: {
     height: 55,
     width: 50,
     fontSize: 32,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    borderBottomColor: '#ddb52f',
+    fontWeight: "bold",
+    textAlign: "center",
+    borderBottomColor: "#ddb52f",
     borderBottomWidth: 2,
-    color: '#ddb52f',
+    color: "#ddb52f",
     marginVertical: 8,
   },
 });
