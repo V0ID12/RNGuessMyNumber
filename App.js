6ebbb3c01cb27add25bf584890f3af7b70a501c4
Donovan Ferrel <dferrel@tsts.com>
2025-01-06 11:40:54 -0600

    Code changes to apply coloring to the components and overall app

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import { StyleSheet } from "react-native";
+import { LinearGradient } from "expo-linear-gradient";
 
 import StartGameScreen from "./screens/StartGameScreen";
 
 export default function App() {
-  return <StartGameScreen />;
+  return (
+    <LinearGradient style={styles.rootScreen} colors={["#4e0329", "#ddb52f"]}>
+      <StartGameScreen />
+    </LinearGradient>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  rootScreen: {
+    flex: 1,
+  },
+});
